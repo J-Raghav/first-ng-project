5b872e6e3c206af4b6f36e14ed069af01d158344
Raghav Joshi <raghavjoshi789@gmail.com>
2021-06-28 13:06:58 +0530

    First commit added two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { TrainerListComponent } from './trainer-list/trainer-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent,
+    TrainerListComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'student-list', component: StudentListComponent},
+      {path: 'trainer-list', component: TrainerListComponent},
+      {path: '', redirectTo: '/student-list', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
